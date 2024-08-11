31983d4cc45a111bb385212c695034b73b137e97
Erioifpud <erioifpud@zuo.si>
2024-08-11 18:28:03 +0800

    feat: 引入 redux

--- options/index.tsx
+++ options/index.tsx
@@ -1,23 +1,27 @@
 import Button from 'antd/es/button'
-import { useState } from "react"
 import { ThemeProvider } from '~theme'
+import { Provider } from "react-redux"
+import { PersistGate } from "@plasmohq/redux-persist/integration/react"
+import { persistor, store } from "~store"
 
 function OptionsPage() {
-  const [data, setData] = useState("")
-
   return (
     <ThemeProvider>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: 16
-        }}>
-        <h1>
-          Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-        </h1>
-        <Button type="primary">Fascinating</Button>
-      </div>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: 16
+            }}>
+            <h1>
+              Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+            </h1>
+            <Button type="primary">Fascinating</Button>
+          </div>
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
   )
 }
