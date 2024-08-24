4b1d28113d570aeff23c2275082fa2fb058a3217
Erioifpud <erioifpud@zuo.si>
2024-08-25 03:23:50 +0800

    feat: 更换页面组件

--- src/options/AppLayout.tsx
+++ src/options/AppLayout.tsx
@@ -1,45 +1,30 @@
-import { ThemeProvider } from '~theme'
 import { Provider } from "react-redux"
 import { PersistGate } from "@plasmohq/redux-persist/integration/react"
 import { persistor, store } from "~store"
-import { Outlet, useLocation, useMatch, useNavigate } from 'react-router-dom'
-import Layout from 'antd/es/layout'
-import { useCallback, memo } from 'react'
-import { Content } from 'antd/es/layout/layout'
+import { Outlet } from 'react-router-dom'
+import { memo } from 'react'
 import '../global.css'
-import { EditOutlined, HomeOutlined } from '@ant-design/icons'
 import { EditContextProvider } from './views/Edit/context'
 
-const items = [
-  { key: '/', icon: <HomeOutlined />, label: '图书馆' },
-  { key: '/edit', icon: <EditOutlined />, label: '站点编辑' },
-]
-
 const AppLayout = memo(() => {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const match = useMatch('/*');
+  // const location = useLocation()
+  // const navigate = useNavigate()
+  // const match = useMatch('/*');
 
-  const to = useCallback((item) => {
-    navigate(item.key, { replace: true })
-  }, [navigate])
+  // const to = useCallback((item) => {
+  //   navigate(item.key, { replace: true })
+  // }, [navigate])
 
   return (
-    <ThemeProvider>
-      <Provider store={store}>
-        <EditContextProvider>
-          <PersistGate loading={null} persistor={persistor}>
-            <Layout style={{ height: '100vh' }}>
-              <Content className="h-full">
-                <div className="relative h-full overflow-hidden">
-                  <Outlet />
-                </div>
-              </Content>
-            </Layout>
-          </PersistGate>
-        </EditContextProvider>
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <EditContextProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <div className="relative h-full overflow-hidden">
+            <Outlet />
+          </div>
+        </PersistGate>
+      </EditContextProvider>
+    </Provider>
   )
 })
 
--- src/options/views/Edit/index.tsx
+++ src/options/views/Edit/index.tsx
@@ -1,24 +1,40 @@
-import { UnorderedListOutlined } from '@ant-design/icons';
-import Button from 'antd/es/button';
-import Drawer from 'antd/es/drawer';
-import { Header } from 'antd/es/layout/layout';
 import { memo, useContext } from 'react';
 import { EditContext } from './context/ctx';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '~/components/ui/sheet'
+import { Button } from '~components/ui/button';
+import { MenuIcon } from 'lucide-react';
 
 const EditPage = memo(() => {
   const { drawerVisible, setDrawerVisible } = useContext(EditContext);
 
   return (
     <div className="relative h-full overflow-hidden flex flex-col">
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <Button type="primary" icon={<UnorderedListOutlined />} onClick={() => {setDrawerVisible(true)}} />
-      </Header>
-      <div className="h-full">EditPage</div>
-      <Drawer title="配置菜单" onClose={() => setDrawerVisible(false)} open={drawerVisible} placement="left">
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Drawer>
+      <div className="flex-shrink-0 h-14 flex items-center px-4 border-b border-solid border-gray-500 bg-gray-700">
+        <Sheet open={drawerVisible} onOpenChange={setDrawerVisible}>
+          <SheetTrigger>
+            <Button size="icon">
+              <MenuIcon></MenuIcon>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="relative flex-grow">EditPage</div>
     </div>
   );
 });
