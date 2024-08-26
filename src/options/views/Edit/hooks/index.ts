e2d1f8cf2447591a7566e3f131d8c1d3c0b93af8
Erioifpud <erioifpud@zuo.si>
2024-08-27 02:24:37 +0800

    feat: 新增 usePages fix: 调整路由匹配规则

--- src/options/views/Edit/hooks/index.ts
+++ src/options/views/Edit/hooks/index.ts
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { useMatch } from 'react-router-dom';
 import type { RootState } from '~store';
 
+// 当前编辑的站点对象
 export const useCurrentSite = () => {
   const match = useMatch('/edit/:id/*');
   const sites = useSelector((state: RootState) => state.site.sites)
@@ -12,12 +13,40 @@ export const useCurrentSite = () => {
   return sites.find(site => site.id === match.params.id)
 }
 
+// 当前编辑的站点 id
 export const useCurrentSiteId = () => {
-  const match = useMatch('/edit/:id/*');
-  return match?.params.id || ''
+  const site = useCurrentSite()
+  return site?.id || ''
 }
 
+// 当前编辑路由的类型
 export const useCurrentEditPage = () => {
-  const match = useMatch('/edit/:id/:type');
+  const match = useMatch('/edit/:id/:type/*');
   return match?.params.type || ''
+}
+
+// 当前编辑的站点 page 对象列表
+export const usePages = () => {
+  const site = useCurrentSite()
+  if (!site) {
+    return null;
+  }
+  return site.pages
+}
+
+// 当前编辑的站点 page 对象
+export const useCurrentPage = () => {
+  const match = useMatch('/edit/*/*/page/:pageId/*');
+  const pages = usePages()
+
+  if (!match || !pages) {
+    return null;
+  }
+  return pages.find(page => page.id === match.params.pageId)
+}
+
+// 当前编辑的站点 page id
+export const useCurrentPageId = () => {
+  const page = useCurrentPage()
+  return page?.id || ''
 }
