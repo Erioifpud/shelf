1e50c5255f30c91ef784544539cd1b0088e7ae0d
Erioifpud <erioifpud@zuo.si>
2024-08-26 22:56:21 +0800

    feat: 新增 page 相关 action

--- src/store/site/site-slice.ts
+++ src/store/site/site-slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 import { randomUUID } from '~utils/uuid'
-import type { Site } from './type'
+import type { Page, Site } from './type'
 
 interface SiteState {
   sites: Site[]
@@ -28,6 +28,46 @@ function createEmptySite(): Site {
   }
 }
 
+function createEmptyPage(): Page {
+  return {
+    id: randomUUID(),
+    title: 'New page',
+    flags: '',
+    query: [],
+    searchParams: '',
+    searchSuggestion: false,
+    list: {
+      url: '',
+      rule: '',
+      displayMode: 'card',
+      headers: '',
+    },
+    detail: {
+      url: '',
+      rule: '',
+      headers: '',
+    },
+    preview: {
+      url: '',
+      rule: '',
+      headers: '',
+    },
+    tag: {
+      url: '',
+      rule: '',
+      displayMode: 'card',
+      headers: '',
+    },
+    search: {
+      url: '',
+      rule: '',
+      displayMode: 'card',
+      headers: '',
+    },
+    subPages: []
+  }
+}
+
 const initialState: SiteState = {
   sites: []
 }
@@ -48,9 +88,42 @@ const siteSlice = createSlice({
         Object.assign(site, action.payload)
       }
     },
+    createPage: (state, action: PayloadAction<string>) => {
+      const site = state.sites.find(site => site.id === action.payload)
+      if (site) {
+        site.pages.push(
+          createEmptyPage()
+        )
+      }
+    },
+    pageMove: (state, action: PayloadAction<{ siteId: string, pageId: string, direction: 'up' | 'down' }>) => {
+      const { siteId, pageId } = action.payload
+      const site = state.sites.find(site => site.id === siteId)
+      if (!site || site.pages.length <= 1) {
+        return
+      }
+      const pageIndex = site.pages.findIndex(page => page.id === pageId)
+      const page = site.pages[pageIndex]
+      if (!page) {
+        return
+      }
+      if (action.payload.direction === 'up') {
+        if (pageIndex === 0) {
+          return
+        }
+        site.pages[pageIndex] = site.pages[pageIndex - 1]
+        site.pages[pageIndex - 1] = page
+      } else {
+        if (pageIndex === site.pages.length - 1) {
+          return
+        }
+        site.pages[pageIndex] = site.pages[pageIndex + 1]
+        site.pages[pageIndex + 1] = page
+      }
+    },
   }
 })
 
-export const { createSite, deleteSite, updateSite } = siteSlice.actions
+export const { createSite, deleteSite, updateSite, createPage, pageMove } = siteSlice.actions
 
 export default siteSlice.reducer
